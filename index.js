a3cbe04ebd7be2b5e103504b3338780173ca0ee6
root <root@LAPTOP-T2LT97UU>
2023-11-01 22:30:07 -0300

    new api integration

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require("express")();
-const port = 8080;
+const port = 8000 ;
 const token = require("./lib/modules/token");
 const register = require("./lib/modules/registerUser");
 const cotacao = require("./lib/cotacao/api/cotacao");
 const info = require("./lib/info/api/info");
+const timezone = require("./lib/timezone/api/timezone");
 
 app.get("/register/:user/:createtoken", (req, res) => {
   const user = req.params.user;
@@ -21,6 +22,7 @@ app.get("/register/:user/:createtoken", (req, res) => {
     }
   }
 });
+
 app.get("/login/:userLogin/:tokenLogin", (req, res) => {
   const userLogin = req.params.userLogin;
   const tokenLogin = req.params.tokenLogin;
@@ -33,30 +35,43 @@ app.get("/login/:userLogin/:tokenLogin", (req, res) => {
     res.send({ error: "User or token incorrect" });
   }
 });
-app.get("/login/:userLogin/:tokenLogin/:pais/:moeda", async (req, res) => {
-  const userLogin = req.params.userLogin;
-  const tokenLogin = req.params.tokenLogin;
-  if (
-    userLogin == register.registerUser.user &&
-    tokenLogin == register.registerUser.token
-  ) {
-    const pais = req.params.pais;
-    const moeda = req.params.moeda;
-    const resultCot = await cotacao.cotGenerate(moeda).then((e) => {
-      return e;
-    });
-    const resultInfo = await info.infoGenerate(pais).then((e) => {
-      return e;
-    });
-    const juncao = {
-      Informacoes: resultInfo,
-      Cotacao: resultCot,
-    };
-    res.json(juncao);
-  } else {
-    res.send({ error: "User or token incorrect" });
+
+app.get(
+  "/login/:userLogin/:tokenLogin/:pais/:moeda/:area/:location",
+  async (req, res) => {
+    const userLogin = req.params.userLogin;
+    const tokenLogin = req.params.tokenLogin;
+    if (
+      userLogin == register.registerUser.user &&
+      tokenLogin == register.registerUser.token
+    ) {
+      const pais = req.params.pais;
+      const moeda = req.params.moeda;
+      const location = req.params.location;
+      const area = req.params.area;
+
+      const resultCot = await cotacao.cotGenerate(moeda).then((e) => {
+        return e;
+      });
+      const resultInfo = await info.infoGenerate(pais).then((e) => {
+        return e;
+      });
+      const resultTime = await timezone
+        .timeGenerate(area, location)
+        .then((e) => {
+          return e;
+        });
+      const integration = {
+        Informacoes: resultInfo,
+        Cotacao: resultCot,
+        Time: resultTime,
+      };
+      res.json(integration);
+    } else {
+      res.send({ error: "User or token incorrect" });
+    }
   }
-});
+);
 app.listen(port, (e) => {
   console.log("Open");
 });
